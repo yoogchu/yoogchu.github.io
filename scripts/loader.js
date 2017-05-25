82e95eb06fd5df36ee30cc61f9fe9be93acf23c4
yoogchu <echu31@gatech.edu>
2017-05-25 01:30:16 -0400

    change loading script

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,3 +1,52 @@
-$(window).load(function() {      
-  $("#loader").fadeOut("slow");  
-});
+function attach(element,listener,ev,tf){
+
+if(element.attachEvent) {
+
+    element.attachEvent("on"+listener,ev);
+
+}else{
+
+    element.addEventListener(listener,ev,tf);
+
+}
+
+}
+
+function fadeOut(element,startLevel,endLevel,duration,callback){
+
+var fOInt;
+
+    op = startLevel;
+
+fOInt = setInterval(function() {
+
+    if(op<=endLevel){
+
+    element.style.opacity = endLevel;
+    element.style.filter = "alpha(opacity = " + endLevel + ")";
+
+    clearInterval(fOInt);
+
+        if(typeof callback == 'function') callback(true);
+
+    }else{
+
+    op -= 0.1;
+
+    element.style.opacity = op;
+    element.style.filter = "alpha(opacity = " + op*100 + ")";
+
+    }
+
+    },duration);
+
+}
+
+attach(window,'load',function(){
+
+    fadeOut(document.getElementById('loader'),1,0,50,function(cb){
+    	document.getElementById('loader').style.display="none";
+    });
+    
+
+},false);
