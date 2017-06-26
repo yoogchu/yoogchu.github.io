7ad5811722c8183d3f500c305cc120ee7a1c9879
yoogchu <echu31@gatech.edu>
2017-06-26 01:06:02 -0400

    self-balancing thumbs if main img is deleted

--- compress.py
+++ compress.py
@@ -5,11 +5,13 @@ from yattag import Doc
 tinify.key = "19UKhWk_BM2XBxQ1akPgWfrPB95fexFw"
 
 DIR = "images/photography/"
+DIR_THUMB = "images/photography/thumbs/"
 EXT = ".jpg"
+increment_num = 0
 
-def countFiles():
+def countFiles(directory):
 	list_dir = []
-	list_dir = os.listdir(DIR)
+	list_dir = os.listdir(directory)
 	count = 0
 	for file in list_dir:
 		if file.endswith(EXT): # eg: '.txt'
@@ -19,21 +21,43 @@ def countFiles():
 # print countFiles(DIR, ".jpg")
 
 def checkRename():
+	global increment_num
+
 	files = glob.glob(DIR + "*" + EXT)
 	files.sort(key=os.path.getmtime)
-	print countFiles()
-	print str(int(files[len(files)-1][files[len(files)-1].rfind('/')+1: files[len(files)-1].rfind('.')]))
+	stop = False
+	i = 1
+	increment_num = len(files)
 
-	if countFiles() == int(files[len(files)-1][files[len(files)-1].rfind('/')+1: files[len(files)-1].rfind('.')]):
-		return False
+	while not stop:
+		try:
+			str(int(files[len(files)-i][files[len(files)-i].rfind('/')+1: files[len(files)-i].rfind('.')]))
+			stop = True
+		except ValueError as e:
+			i=i+1
 
-	else:
-		return True
+	increment_num = increment_num - i + 1
+	return countFiles(DIR) == int(files[len(files)-i][files[len(files)-i].rfind('/')+1: files[len(files)-i].rfind('.')])
+
+def syncDIR():
+	synced = False
+	files = glob.glob(DIR_THUMB + "*" + EXT)
+	files.sort(key=os.path.getmtime)
+	for file in files:
+		num = file[file.rfind('/')+1:file.rfind('_')]
+
+		if not os.path.isfile(DIR + num + EXT):
+			print 'removing ' + file
+			os.remove(file)
+			synced = True
+
+	return synced
+
 def compress():
 	if not os.path.exists(DIR + 'thumbs/'):
 		os.makedirs(DIR + 'thumbs/')
 
-	for i in range (1, countFiles()+1):
+	for i in range (1, countFiles(DIR)+1):
 		if os.path.isfile(DIR + "thumbs/" + str(i) + "_thumb" + EXT):
 			# print 'skipping ' + str(i) + EXT
 			continue
@@ -48,19 +72,22 @@ def compress():
 
 	print 'done'
 
-def rename():
-	numFiles = countFiles()
+def rename(directory):
+	numFiles = countFiles(directory)
 
-	files = glob.glob(DIR + "*" + EXT)
+	files = glob.glob(directory + "*" + EXT)
 	files.sort(key=os.path.getmtime)
 
 	i=1
 	for image in files:
-		os.rename(image, DIR + str(i) + EXT)
+		if directory == DIR_THUMB:
+			os.rename(image, directory + str(i) + "_thumb" + EXT)
+		else:
+			os.rename(image, directory + str(i) + EXT)
 		i+=1
 
 def createTags():
-	numFiles = countFiles()
+	numFiles = countFiles(DIR)
 	doc, tag, text = Doc().tagtext()
 
 	with tag('div', klass='container-images'):
@@ -78,13 +105,20 @@ def writeToHTML(filename):
 	overWrite.close()
 
 def main():
-	if checkRename():
-		print 'Renaming ' + str(countFiles()) + ' files ...'
-		rename()
+	if syncDIR():
+		print 'Renaming main dir ...'
+		rename(DIR)
+		print 'Renaming thumbs ...'
+		rename(DIR_THUMB)
+
+	checkRename()
+	if increment_num < countFiles(DIR):
+		print 'Renaming ' + str(countFiles(DIR)) + ' files ...'
+		rename(DIR)
 		print 'Writing to HTML ...'
 		writeToHTML('photography.html')
-	print 'Compressing ...'
-	compress()
+		print 'Compressing ...'
+		compress()
 	
 if __name__ == '__main__':
 	main()
