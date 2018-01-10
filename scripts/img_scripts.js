1e8b6a369472b4ae4297f7705dfeb2e97730b9a1
yoogchu <echu31@gatech.edu>
2018-01-10 13:00:15 -0500

    fix cross-browser bug

--- scripts/img_scripts.js
+++ scripts/img_scripts.js
@@ -4,9 +4,6 @@ var lastClickedImage = null;
 var fullScreen = false;
 
 document.onkeydown = function(e) {
-	// if (e.keyCode == 187) {
-	// 	critique();
-	// } else if (fullScreen == false) {
 	if (fullScreen == false) {
 		return;
 	} else {
@@ -14,18 +11,6 @@ document.onkeydown = function(e) {
 	}
 };
 
-// function critique() {
-// 	var images = document.querySelectorAll(".container-images > img");
-// 	// console.log(images);
-// 	for (var i = images.length-1; i > 0; i--) {
-// 		if (critique_photos.includes(i)) {
-// 			// console.log(images[images.length-i].currentSrc);
-// 			continue;
-// 		} else {
-// 			images[images.length-i].style.visibility="hidden";
-// 		}
-// 	}
-// }
 function changeImage(e) {
 	try {
 		code = e.keyCode;
@@ -42,11 +27,11 @@ function changeImage(e) {
 			num:fs_source.substring(fs_source.lastIndexOf("/")+1, fs_source.lastIndexOf(".")),
 			ext: fs_source.substring(fs_source.lastIndexOf(".")+1)
 		}
-	// var images = document.querySelectorAll(".container-images > img");
 
-	// first_img = images[0].src.substring(images[0].src.lastIndexOf("/")+1, images[0].src.lastIndexOf("_"));
 	var images = document.querySelectorAll(".image");
-	first_img = images[0].style.backgroundImage.split('"')[1].substring(images[0].style.backgroundImage.split('"')[1].lastIndexOf("/")+1, images[0].style.backgroundImage.split('"')[1].lastIndexOf("_"));
+	first_img_str = images[0].style.backgroundImage.split('(')[1].split(')')[0];
+	console.log(first_img_str);
+	first_img = first_img_str.substring(first_img_str.lastIndexOf("/")+1, first_img_str.lastIndexOf("_"));
 	switch (code) {
 		case 27: //esc
 			hideFS()
@@ -87,10 +72,17 @@ function clickImage(id) {
 	var clickedImage = id;
 	// var src = clickedImage.src;
 	console.log(clickedImage);
-	var src = clickedImage.style.backgroundImage.split('"')[1];
+
+	var src = clickedImage.style.backgroundImage.split('(')[1].split(')')[0];
 	console.log(src);
+
 	var newSrc = src.substring(0, src.indexOf("thumbs")) + src.substring(src.indexOf("thumbs")+7,
 		src.indexOf("_thumb")) + src.substring(src.indexOf("_thumb")+6, src.length);
+	console.log(newSrc);
+	if (newSrc.indexOf('"') != -1) {
+		newSrc = newSrc.substring(1,newSrc.length-1);
+	}
+	console.log(newSrc);
 	document.getElementById("fs-image").src = newSrc;
 
 	if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
